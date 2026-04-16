b01ef69a652b35d0ec3704a067d9e2dde7cfb56d
IndicareLeve <daniele@indicareleve.me>
2026-04-16 11:12:29 +0200

    feat: add embedded templates and basic routing

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 
@@ -9,13 +11,35 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+//go:embed templates/*
+var templatesFS embed.FS
+
+var tmpl *template.Template
+
+func init() {
+	var err error
+	tmpl, err = template.ParseFS(templatesFS, "templates/*.html", "templates/fragments/*.html")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing templates: %v\n", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Quadge - Quadlet Manager"))
+		data := map[string]interface{}{
+			"Services": []interface{}{},
+			"Selected": "",
+		}
+		tmpl.ExecuteTemplate(w, "index.html", data)
+	})
+
+	r.Get("/new", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "new.html", nil)
 	})
 
 	port := os.Getenv("QUADGE_PORT")
